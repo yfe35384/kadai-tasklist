bffb62063536dbfe0910ca519a20b66ff91b702f
Nango <yfe35384@yahoo.co.jp>
2021-02-16 06:26:41 +0900

    Add getAllTasklist

--- src/models/Tasklist.java
+++ src/models/Tasklist.java
@@ -7,9 +7,17 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 
 @Entity
+@NamedQueries({
+    @NamedQuery(
+        name = "getAllTasklist",
+        query = "SELECT m FROM Tasklist AS m ORDER BY m.id DESC"
+    )
+})
 @Table(name = "tasklist")
 public class Tasklist {
     @Id
