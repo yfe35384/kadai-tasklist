8867a51aa1e139f80d6f3788e6b0d9b8d347166d
Nango <yfe35384@yahoo.co.jp>
2021-02-16 20:41:08 +0900

    Add DestroyServlet.java

---
+++ src/controllers/DestroyServlet.java
@@ -0,0 +1,49 @@
+package controllers;
+
+import java.io.IOException;
+
+import javax.persistence.EntityManager;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import models.Tasklist;
+import utils.DBUtil;
+
+/**
+ * Servlet implementation class DestroyServlet
+ */
+@WebServlet("/destroy")
+public class DestroyServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public DestroyServlet() {
+        super();
+    }
+
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String _token = request.getParameter("_token");
+        if(_token != null && _token.equals(request.getSession().getId())) {
+            EntityManager em = DBUtil.createEntityManager();
+
+            Tasklist m = em.find(Tasklist.class, (Integer)(request.getSession().getAttribute("tasklist_id")));
+
+            em.getTransaction().begin();
+            em.remove(m);
+            em.getTransaction().commit();
+            em.close();
+
+            request.getSession().removeAttribute("tasklist_id");
+
+            response.sendRedirect(request.getContextPath() + "/index");
+        }
+    }
+}
